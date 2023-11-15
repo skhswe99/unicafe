eed4b31ab2b45e4852b530defd66aaa8e476ce9a
skhswe99 <skhswe99@gmail.com>
2023-11-15 13:47:44 -0800

    complete functionality to display feedback count

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import { useState } from 'react'
+import './App.css'
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => {
+    const newGoodCount = good + 1
+    setGood(newGoodCount)
+  }
+
+  const handleNeutral = () => {
+    const newNeutralCount = neutral + 1
+    setNeutral(newNeutralCount)
+  }
+
+  const handleBad = () => {
+    const newBadCount = bad + 1
+    setBad(newBadCount)
+  }
   return (
     <div>
-      code here
+      <p>give feedback</p>
+      <button onClick={handleGood} id='good-button'>good</button>
+      <button onClick={handleNeutral} id='neutral-button'>neutral</button>
+      <button onClick={handleBad} id='bad-button'>bad</button>
+      <p>statistics</p>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}<br/>
     </div>
 )
 }
